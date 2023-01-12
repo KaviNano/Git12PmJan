8f10899fdbcad91d4969f95670fbc379e410b09c
KaviNano <kavinano100@gmail.com>
2023-01-12 12:55:11 +0530

    karthik doone

--- src/com/abc/Sample.java
+++ src/com/abc/Sample.java
@@ -5,4 +5,10 @@ public class Sample {
 	private void tc() {
 		System.out.println("Testcase");
 	}
+	
+	public static void main(String[] args) {
+		
+		int age = 22;
+		System.out.println(age);
+	}
 }
